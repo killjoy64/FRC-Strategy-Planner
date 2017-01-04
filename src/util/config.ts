a5537d0700f8e7a1e90a16c5094affa4ff85779c
Kyle Flynn <kyle.flynn5798@yahoo.com>
2017-01-04 03:47:22 -0500

    Upated resources.

--- src/util/config.ts
+++ src/util/config.ts
@@ -2,7 +2,7 @@ export class Config {
 
   public static VERSION: string = "2.2.0";
 
-  public static IS_BROWSER: boolean = true;
+  public static IS_BROWSER: boolean = false;
   public static TEAM_NUMBER: number = null;
 
   constructor() {}
