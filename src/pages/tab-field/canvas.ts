57aaf879037020ae002c1cebb0eeff8e7792b3a1
Kyle Flynn <kyle.flynn5798@yahoo.com>
2017-01-31 00:53:28 -0500

    Added field menu items back.

--- src/pages/tab-field/canvas.ts
+++ src/pages/tab-field/canvas.ts
@@ -4,6 +4,7 @@
 
 export class Canvas {
 
+  menu: any;
   canvas: any;
   context: any;
   content: any;
@@ -11,6 +12,7 @@ export class Canvas {
   img: any;
 
   constructor(content, img) {
+    this.menu = document.getElementById("canvas-menu");
     this.canvas = document.querySelector("canvas");
     this.context = this.canvas.getContext("2d");
     this.content = content;
@@ -25,6 +27,9 @@ export class Canvas {
     this.canvas.setAttribute("height", totalHeight + "px");
     this.canvas.setAttribute("width", totalWidth + "px");
     this.canvas.style.left = this.img.offsetLeft + "px";
+
+    this.canvas.style.top = this.menu.clientHeight + "px";
+    this.canvas_img.style.marginTop = this.menu.clientHeight + "px";
   }
 
 }
--- src/pages/tab-field/tab-field.ts
+++ src/pages/tab-field/tab-field.ts
@@ -21,6 +21,8 @@ export class FieldPage {
   content: any;
   canvas_img: any;
 
+  last_palette: any;
+
   constructor(public navCtrl: NavController) {
     this.connection = new ConnectionManager();
   }
@@ -32,6 +34,79 @@ export class FieldPage {
     setTimeout(() => {
       this.canvas_manager.resize();
     }, 100);
+  }
+
+  public openViewPalette() {
+    this.last_palette = "view-palette";
+    this.resetPalettes();
+    document.getElementById("view-btn").classList.add("active-menu");
+  }
+
+  public openDrawPalette() {
+    this.resetPalettes();
+    this.openPalette("draw-palette");
+    document.getElementById("draw-btn").classList.add("active-menu");
+  }
+
+  public openFieldPalette() {
+    this.resetPalettes();
+    this.openPalette("field-palette");
+    document.getElementById("field-btn").classList.add("active-menu");
+  }
+
+  public openRobotPalette() {
+    this.resetPalettes();
+    this.openPalette("robot-palette");
+    document.getElementById("robot-btn").classList.add("active-menu");
+  }
+
+  public openSavePalette() {
+    this.last_palette = "save-palette";
+    this.resetPalettes();
+    document.getElementById("save-btn").classList.add("active-menu");
+    this.saveCanvas();
+  }
+
+  public openFilePalette() {
+    this.last_palette = "file-palette";
+    this.resetPalettes();
+    document.getElementById("open-btn").classList.add("active-menu");
+    this.openFileModal();
+  }
+
+  private resetPalettes() {
+    let palettes = document.getElementsByClassName("palette");
+
+    for (let i = 0; i < palettes.length; i++) {
+      palettes[i].classList.remove("palette-down");
+      palettes[i].classList.add("palette-up");
+    }
+
+    let buttons = document.getElementsByClassName("menu-button");
+
+    for (let i = 0; i < buttons.length; i++) {
+      buttons[i].classList.remove("active-menu");
+    }
+  }
+
+  private openPalette(id) {
+    this.resetPalettes();
+
+    if (this.last_palette != id) {
+      let palette = document.getElementById(id);
+      palette.classList.remove("palette-up");
+      palette.classList.add("palette-down");
+    }
+
+    this.last_palette = id;
+  }
+
+  saveCanvas() {
+
+  }
+
+  openFileModal() {
+
   }
 
 }
